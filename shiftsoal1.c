70c8420a4b2f1c1f644f7ac5c8da343a94b24204
bagasimmanuel <55182200+bagasimmanuel@users.noreply.github.com>
2020-03-13 14:37:36 +0700

    ShiftSoal 1

---
+++ shiftsoal1.c
@@ -0,0 +1,97 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+  pid_t pid, sid;        // Variabel untuk menyimpan PID
+  int sec,min,hour;
+
+  pid = fork();     // Menyimpan PID dari Child Process
+
+  /* Keluar saat fork gagal
+  * (nilai variabel pid < 0) */
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  /* Keluar saat fork berhasil
+  * (nilai variabel pid adalah PID dari child process) */
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if ((chdir("/")) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+
+  if(argc != 5){
+    printf("Argumen harus 5");
+    return 0;
+  }
+
+  sec = 0;
+  min = 0;
+  hour = 0;
+  //Argument Detik
+  if(argv[1][0] == '*'){
+    sec = 0;
+  }else if(isalpha(argv[1][0])){
+    printf("Harus angka");
+  }else if(atoi(argv[1]) < 0 || atoi(argv[1]) > 59){
+    printf("Range 0 - 59");
+  }else{
+    sec = atoi(argv[1]);
+  }
+  //Argument Menit
+  if(argv[2][0] == '*'){
+    min = 0;
+  }else if(isalpha(argv[2][0])){
+    printf("Harus angka");
+  }else if(atoi(argv[2]) < 0 || atoi(argv[2]) > 59){
+    printf("Range 0 - 59");
+  }else{
+    min = atoi(argv[2]);
+  }
+  //Argument Jam
+  if(argv[3][0] == '*'){
+    hour = 0;
+  }else if(isalpha(argv[3][0])){
+    printf("Harus angka");
+  }else if(atoi(argv[3]) < 0 || atoi(argv[3]) > 24){
+    printf("Range 0 - 24");
+  }else{
+    hour = atoi(argv[3]);
+  }
+
+  // if(argv[2][0] == '*') min = atoi(argv[2]);
+  // if(argv[3][0] == '*') hour = atoi(argv[3]);
+
+
+  while(1) {
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    if((hour == tm.tm_hour || hour == 0) && (min == tm.tm_min || min == 0) && (sec == tm.tm_sec || sec == 0)){
+      if(fork()==0){
+        char *argx[] = {"bash",argv[4], NULL};
+        execv("/bin/bash", argx);
+      }
+    }
+    sleep(1);
+  }
+}
