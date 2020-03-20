ba2a18110a6776de44c13f96504e6b22ffa4bc5e
bagasimmanuel <bagasimmanuel00@gmail.com>
2020-03-20 08:55:51 +0000

    Revisi :)

--- Soal1/andrew.sh
+++ Soal1/andrew.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir tes
+date >> date.txt
--- Soal1/shiftsoal11.c
+++ Soal1/shiftsoal11.c
@@ -40,36 +40,42 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
-  sec = 0;
-  min = 0;
-  hour = 0;
+  sec = -1;
+  min = -1;
+  hour = -1;
   //Argument Detik
   if(argv[1][0] == '*'){
-    sec = 0;
+    sec = -1;
   }else if(isalpha(argv[1][0])){
     printf("Harus angka");
+    return 0;
   }else if(atoi(argv[1]) < 0 || atoi(argv[1]) > 59){
     printf("Range 0 - 59");
+    return 0;
   }else{
     sec = atoi(argv[1]);
   }
   //Argument Menit
   if(argv[2][0] == '*'){
-    min = 0;
+    min = -1;
   }else if(isalpha(argv[2][0])){
     printf("Harus angka");
+    return 0;
   }else if(atoi(argv[2]) < 0 || atoi(argv[2]) > 59){
     printf("Range 0 - 59");
+    return 0;
   }else{
     min = atoi(argv[2]);
   }
   //Argument Jam
   if(argv[3][0] == '*'){
-    hour = 0;
+    hour = -1;
   }else if(isalpha(argv[3][0])){
     printf("Harus angka");
+    return 0;
   }else if(atoi(argv[3]) < 0 || atoi(argv[3]) > 24){
     printf("Range 0 - 24");
+    return 0;
   }else{
     hour = atoi(argv[3]);
   }
@@ -81,7 +87,7 @@ int main(int argc, char *argv[]) {
   while(1) {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    if((hour == tm.tm_hour || hour == 0) && (min == tm.tm_min || min == 0) && (sec == tm.tm_sec || sec == 0)){
+    if((hour == tm.tm_hour || hour == -1) && (min == tm.tm_min || min == -1) && (sec == tm.tm_sec || sec == -1)){
       if(fork()==0){
         char *argx[] = {"bash",argv[4], NULL};
         execv("/bin/bash", argx);
